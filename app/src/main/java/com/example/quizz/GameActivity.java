5781a9fb53cb0b52e60612c96faa4c0c9b274d9b
clementseiler <clement70200@gmail.com>
2022-11-30 18:23:07 +0100

    intent start

--- app/src/main/java/com/example/quizz/GameActivity.java
+++ app/src/main/java/com/example/quizz/GameActivity.java
@@ -3,12 +3,27 @@ package com.example.quizz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class GameActivity extends AppCompatActivity {
+
+    private TextView mQuestionView;
+    private Button mAnswerOne;
+    private Button mAnswerTwo;
+    private Button mAnswerThree;
+    private Button mAnswerFour;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        mQuestionView = findViewById(R.id.game_activity_textview_question);
+        mAnswerOne = findViewById(R.id.game_activity_button_1);
+        mAnswerTwo = findViewById(R.id.game_activity_button_2);
+        mAnswerThree = findViewById(R.id.game_activity_button_3);
+        mAnswerFour = findViewById(R.id.game_activity_button_4);
+
     }
 }
--- app/src/main/java/com/example/quizz/MainActivity.java
+++ app/src/main/java/com/example/quizz/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.quizz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -48,7 +49,8 @@ public class MainActivity extends AppCompatActivity {
         mPlayButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.d("btn","Click ok");
+                Intent gameActivityIntent = new Intent(MainActivity.this,GameActivity.class);
+                startActivity(gameActivityIntent);
             }
         });
     }
